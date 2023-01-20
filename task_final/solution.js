763bb10b25054db054b051a2c5705f5a9450e69a
Rinat test <rinat11.08@mail.ru>
2023-01-20 20:16:28 +0300

    commit task final.1.2

--- task_final/solution.js
+++ task_final/solution.js
@@ -42,12 +42,14 @@ function sendRequest(name, phone, address, goods, sum) {
         data.goods.push({title: goods[i].title, count: goods[i].count});
     }
 
-    data.order.address = "ул. " `${address.street}` + ", дом " + `${address.house}` + " , " + `${address.entrance}` + " подъезд," + `${address.floor}` + " этаж, " + "кв " + `${address.flat}`;
+    1
+data.order.address = `ул. ${address.street}, дом ${address.house}, ${address.entrance} подъезд, ${address.floor} этаж, кв. ${address.flat}`;
     data.order.sum = sum;
 
     data.client = `${name}` + " " + `${phone}`;
 
-    let jsonData = JSON.stringify({data});
+    let jsonData = JSON.stringify({data: data});
+
 
     return jsonData;
 }
